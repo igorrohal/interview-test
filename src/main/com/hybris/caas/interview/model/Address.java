2aff7165c5cbb86c1e3887d3c1ed67430bee84d9
Igor Rohal <igor.rohal@gmail.com>
2016-07-14 17:00:21 +0200

    initial commit of code

---
+++ src/main/com/hybris/caas/interview/model/Address.java
@@ -0,0 +1,70 @@
+package com.hybris.caas.interview.model;
+
+/**
+ * @author SAP Hybris YaaS
+ */
+public class Address {
+    String id;
+    String street;
+    String streetNumber;
+    String zipCode;
+    String city;
+    String state;
+    String country;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
---
+++ src/test/com/hybris/caas/interview/service/ExampleSolutionDefaultAddressServiceTest.java
@@ -0,0 +1,10 @@
+package com.hybris.caas.interview.service;
+
+/**
+ * @author the interviewee :)
+ */
+public class ExampleSolutionDefaultAddressServiceTest {
+
+    // TODO: write unit tests here
+
+}
